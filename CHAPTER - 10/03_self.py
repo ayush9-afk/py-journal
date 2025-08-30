ffcfffc75daa162d30b8be4c9b981b4eb246a861
Ayush Kumar <im.mrayush9@gmail.com>
2025-08-30 11:04:06 +0530

    Improved Employee class

--- CHAPTER - 10/03_self.py
+++ CHAPTER - 10/03_self.py
@@ -2,13 +2,17 @@ class Employee:
     language = "Python"#This is class attribute
     salary = 1200000
 
+    def set_name(self, name):
+        self.name = name
+
     def getInfo(self):
         print(f"The language is {self.language}. The salary is {self.salary}")
 
-    #def greed(self):
-        #print("Good morning")
+    def greet(self):
+        print(f"Good morning {self.name}")
 
-harry = Employee()
-#harry.greed()
-harry.getInfo()
-#Employee.getInfo(harry)
+emp1 = Employee()
+emp1.set_name("Harry")
+emp1.greet()
+emp1.getInfo()
+# Employee.getInfo(emp1) This is also Equivalent to the above line
