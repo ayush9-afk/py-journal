3a8b5def1d494443e707aca9d361ad4a764becfc
Ayush Kumar <im.mrayush9@gmail.com>
2025-08-31 23:52:03 +0530

    Added new method in employee class

--- CHAPTER - 10/03_self.py
+++ CHAPTER - 10/03_self.py
@@ -4,10 +4,15 @@ class Employee:
 
     def getInfo(self):
         print(f"The language is {self.language}. The salary is {self.salary}")
+    
+    def update_salary(self, salary):
+        self.salary = salary
 
     # def greet(self):
     #     print(f"Good morning")
 
 emp1 = Employee()
 emp1.getInfo()
+Employee.getInfo(emp1) # This is also Equivalent to the above line
+emp1.update_salary(2000000)
 Employee.getInfo(emp1) # This is also Equivalent to the above line
