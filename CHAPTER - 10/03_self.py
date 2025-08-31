ff5c3704eb553692b8325c0a983f13709d8e76da
Ayush Kumar <im.mrayush9@gmail.com>
2025-08-31 23:46:05 +0530

    removed Chapter -9

--- CHAPTER - 10/03_self.py
+++ CHAPTER - 10/03_self.py
@@ -2,17 +2,12 @@ class Employee:
     language = "Python"#This is class attribute
     salary = 1200000
 
-    def set_name(self, name):
-        self.name = name
-
     def getInfo(self):
         print(f"The language is {self.language}. The salary is {self.salary}")
 
-    def greet(self):
-        print(f"Good morning {self.name}")
+    # def greet(self):
+    #     print(f"Good morning")
 
 emp1 = Employee()
-emp1.set_name("Harry")
-emp1.greet()
-# emp1.getInfo()
-# Employee.getInfo(emp1) # This is also Equivalent to the above line
+emp1.getInfo()
+Employee.getInfo(emp1) # This is also Equivalent to the above line
