0a3f52473d70cc81cb60ed7b6dab2c496b925a2e
ayush9-afk <immr.ayush9@gmail.com>
2025-09-05 18:41:39 +0530

    Added new feature

--- CHAPTER - 10/03_self.py
+++ CHAPTER - 10/03_self.py
@@ -7,9 +7,18 @@ class Employee:
 
     def greet(self):
         print(f"Good morning")
+    
+    def get_joining_date(self):
+        return self.joinoing_date
+    
+    def set_joining_date(self, joining_date: str):
+        self.joinoing_date = joining_date
+    
 
 harry = Employee()
-# harry.language = "JavaScript" # This is an instance attribute
+harry.language = "JavaScript" # This is an instance attribute
+harry.set_joining_date("9th August 2006")
+print(harry.get_joining_date())
 harry.greet()
 harry.getInfo()
-# Employee.getInfo(harry)
+Employee.getInfo(harry)
